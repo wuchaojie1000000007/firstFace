255c373d8c3c8c9f08ebd437a984b565b725a38c
Chaojie Wu <wuchaojie925@gmail.com>
2021-05-07 09:25:32 +0800

    Initial Commit

---
+++ firstFace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstFace
+//
+//  Created by chaojie wu on 2021/5/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
